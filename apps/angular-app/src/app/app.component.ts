fd3dacd7430cd53703fa11fd61005787501e69ea
Rushi Patel <rp970.patel@gmail.com>
2021-11-28 19:57:11 +1030

    chore: add angular app

---
+++ apps/angular-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'poc-nx-workspace-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'angular-app';
+}
